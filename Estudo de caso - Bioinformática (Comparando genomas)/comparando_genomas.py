97ede0e0b2085853503f9eb49b691afed4c45497
lucaandradet <64807231+lucaandradet@users.noreply.github.com>
2021-04-19 12:19:52 -0300

    Update comparando_genomas.py

--- Estudo de caso - Bioinformática (Comparando genomas)/comparando_genomas.py
+++ Estudo de caso - Bioinformática (Comparando genomas)/comparando_genomas.py
@@ -1,5 +1,5 @@
-entrada = open("16s_bacteria.fasta").read()
-saida = open("bacteria.html","w")
+entrada = open("18s_humano.fasta").read()
+saida = open("18s_humano.html","w")
 
 cont = {}
 
@@ -16,4 +16,12 @@ for k in range(len(entrada)-1):
 
 i = 1
 for k in cont:
-    saida.write()
+    transparencia = cont[k]/max(cont.values())
+    saida.write("<div style='width: 100px; border:1px solid #111; height:100px; float:left; background-color: rgba(0,0, 255,"+str(transparencia)+"')>"+k+"</div>")
+    
+    if i%4 == 0:
+        saida.write("<div style='clear:both'></div>")
+    
+    i+=1
+
+saida.close()
